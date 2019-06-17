98ccebb766eb89efd8e51ec9fa369a4fc1e76738
Chathan Driehuys <chathan@driehuys.com>
2019-06-17 19:19:02 -0400

    Set up python package structure.

---
+++ email_auth/apps.py
@@ -0,0 +1,10 @@
+from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
+
+
+class EmailAuthConfig(AppConfig):
+    """
+    Default configuration for the ``email_auth`` package.
+    """
+    name = 'email_auth'
+    verbose_name = _('Simple Email Authentication')
---
+++ setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+
+
+def get_description():
+    with open("README.rst") as f:
+        return f.read()
+
+
+setup(
+    # Package meta-data
+    name="django-simple-email-auth",
+    version="0.0.0",
+    description="Django app for email based authentication and registration.",
+    long_description=get_description(),
+    author="Chathan Driehuys",
+    author_email="chathan@driehuys.com",
+    url="https://github.com/cdriehuys/django-simple-email-auth",
+    license="MIT",
+    # Additional classifiers for PyPI
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        # Supported versions of Django
+        "Framework :: Django",
+        "Framework :: Django :: 1.11",
+        "Framework :: Django :: 2.0",
+        "Framework :: Django :: 2.1",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        # Supported versions of Python
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    # Include the actual source code
+    include_package_data=True,
+    packages=find_packages(),
+    # Dependencies
+    install_requires=[
+        "Django >= 1.10",
+    ],
+)
