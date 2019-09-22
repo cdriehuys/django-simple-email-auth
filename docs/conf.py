4dc44d583f99cd525cc3c19c4cbef0addee3e894
Chathan Driehuys <chathan@driehuys.com>
2019-09-22 14:13:34 -0400

    Bump version: 0.3.0 → 0.3.1

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2019, Chathan Driehuys"
 author = "Chathan Driehuys"
 
 # The full version, including alpha/beta/rc tags
-release = "v0.3.0"
+release = "v0.3.1"
 
 
 # -- General configuration ---------------------------------------------------
