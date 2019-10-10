6e59a6087340ab7480a4f633e1b8d23034e4c31a
Chathan Driehuys <chathan@driehuys.com>
2019-10-09 21:17:59 -0400

    chore: Count 'check' in coverage report.
    Count the Django system check under our coverage report. This correctly
    reports our admin configuration as tested since errors there will be
    caught by the 'manage.py check' command.

--- test_urls.py
+++ test_urls.py
@@ -1,3 +1,18 @@
+from django.contrib import admin
 from django.urls import include, path
 
-urlpatterns = [path("rest/", include("email_auth.interfaces.rest.urls"))]
+try:
+    import rest_framework  # noqa
+
+    REST_FRAMEWORK_AVAILABLE = True
+except ImportError:
+    REST_FRAMEWORK_AVAILABLE = False
+
+# Include the admin URL so 'manage.py check' actually does something.
+urlpatterns = [path("admin/", admin.site.urls)]
+
+# If DRF is installed, include the REST endpoints.
+if REST_FRAMEWORK_AVAILABLE:
+    urlpatterns.append(
+        path("rest/", include("email_auth.interfaces.rest.urls"))
+    )
